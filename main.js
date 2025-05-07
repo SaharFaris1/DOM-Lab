248838a965b22ab5e1585b56c1567ece1d42de67
Sahar Faris <saharfaris@Sahars-Laptop.local>
2025-05-07 11:05:24 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let text=document.getElementsByClassName("title")[0]
+text.style.color="red"
+text.style.background="blue"
+
+let p1=document.getElementById("item")
+p1.style.fontWeight = "bold";
+
+
+document.getElementsByTagName("img")[0];
+img.src = "woman.png";
+img.style.height = "50px";
+
+
+let list = document.querySelector('.horizontal-list');
+let items = ['item 1', 'item 2', 'item 3'];
+
+
+let table = document.getElementById("myTable");
